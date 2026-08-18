6a2d35635bb7a1e21e8773046ada106d12307f3d
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-08-18 14:26:26 +0900

    13-타입 지정 방법1

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Posts() {
-    return <div>글 목록</div>;
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/v1/posts")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            setPosts(data);
+        });
+    },[]);
+
+    return <div>
+    <h1>글 목록</h1>
+    <ul>
+        {posts.map((post: {id: number, title: string}) => {
+            return <li key={post.id}>ID: {post.id}, Title: {post.title}</li>
+        })}
+    </ul>
+    </div>;
 }
